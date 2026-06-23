bc6c3bac706e481d58a98e9e356ae1ece7c79a9c
refugee8888 <paulostaci88@gmail.com>
2026-06-23 13:37:22 +0300

    added file content read

---
+++ functions/get_file_content.py
@@ -0,0 +1,31 @@
+import os
+from typing import Mapping
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    lines = []
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        # Will be True or False
+        valid_target_file = (
+            os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+        )
+        if not valid_target_file:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    except Exception as e:
+        return f"Error: {e}"
+
+    with open(target_file, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+        if f.read(1):
+            file_content_string += (
+                f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            )
+
+    return file_content_string
